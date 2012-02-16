9778cbef77a54e1aa609d19f0807e94e195c6bad
Loris Fichera <loris.fichera@gmail.com>
2012-02-16 16:27:39 +0100

    added network class + something else in the Node. Still immature.

--- pyhtm/pyhtm.py
+++ pyhtm/pyhtm.py
@@ -3,6 +3,7 @@ pyhtm.py
 An implementation of Hierarchical Temporal Memory in Python.
 """
 
+import time
 from numpy import *
 
 class Clock (object):
@@ -17,16 +18,53 @@ class Clock (object):
     def step (self): self.__time = self.__time + 1
     def now (self): return self.__time
     def reset (self): self.__time = 0
-    
+
+
+def link (uChild, uParent):
+    uChild.parent = uParent
+    uParent.children.append (uChild)
+
 
 class Node (object):
     def __init__ (self, uName = None, *args, **kwargs):
+        """The Node class. """
         self._input_vector = {} ## an empty dict
+        self._y = [] ## densities over coincidences
+        self._lambda_plus = [] ## densities over temporal groups
+        
         self._PCG = array ([[]]) ## an empty matrix
         self._coincidences = array ([[]]) ## an empty vector of vectors
         self._temporal_groups = set ([])
-        self._name = uName
-
-    def feed_input (self, uChild, uLambda):
-        self._input_vector[uChild] = uLambda
         
+        self.receptive_field_begin = 0
+        self.receptive_field_end = 0
+        
+        self.name = uName
+        self.children = []
+        self.parent = None
+        
+
+    def feed (self, uLambda, uTime):
+        pass
+        
+        
+
+
+def Network (object):
+    """The Network class. Contains nodes arranged in a hierarchy."""
+    def __init__ (self, *args, **kwargs):
+        self.layers = {}
+        self.nodes = {}
+
+    def feed (self, uInput, uTime = time.time()):
+        ## transform the input into a flattened list
+        flattened_input = [item for sublist in uInput for item in sublist]
+
+        for node in self.layers[0]:
+            node.feed (flattened_input [node.receptive_field_begin,
+                                        node.receptive_field_end],
+                       uTime)
+
+    # ## let the '[]' operator on instances of this class
+    # def __getitem__(self, attr):
+    #     return self.__dict__[attr]
