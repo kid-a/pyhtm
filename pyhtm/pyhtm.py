a5c667fabe07c6ad09b43a4e492cd837052b7f28
Loris Fichera <loris.fichera@gmail.com>
2012-02-17 18:03:08 +0100

    added load and save to YAML

--- pyhtm/pyhtm.py
+++ pyhtm/pyhtm.py
@@ -6,6 +6,7 @@ An implementation of Hierarchical Temporal Memory in Python.
 import time
 from numpy import *
 from operator import mul
+import yaml
 
 
 class Clock (object):
@@ -61,7 +62,6 @@ class Node (object):
             self._y.append (reduce (mul, selected_features))
 
         ## compute density over temporal groups
-        print self._y
         self._lambda_plus = dot( array (self._y), self._PCG)
 
     def propagate (self):
@@ -70,7 +70,8 @@ class Node (object):
 
 class Network (object):
     """The Network class. Contains nodes arranged in a hierarchy."""
-    def __init__ (self, *args, **kwargs):
+    def __init__ (self, uName = None, *args, **kwargs):
+        self.name = uName
         self.layers = {}
         self.nodes = {}
 
@@ -87,5 +88,77 @@ def link (uChild, uParent):
                 
 class NetworkBuilder (object):
     """A NetworkBuilder class. Implements methods to build complex networks."""
-    def __init__ (self, *args, **kwargs):
-        pass
+    def load (self, uFile):
+        f = open (uFile)
+        content = f.read ()
+        f.close ()
+
+        net_descr = yaml.load (content)
+        network = Network (uName = net_descr['name'])
+        
+        for layer in net_descr['layers']:
+            [nodes] = layer.values ()
+            [layer_name] = layer.keys ()
+
+            list_of_nodes = []
+            for node_name in nodes:
+                n = Node(uName = node_name)
+                list_of_nodes.append (n)
+                network.nodes[node_name] = n
+            
+            network.layers[layer_name] = list_of_nodes
+
+        for node in net_descr['nodes']:
+            [node_name] = node.keys()
+            for (attribute_name, attribute_value) in node[node_name].iteritems():
+                if attribute_name == 'coincidences' and \
+                        attribute_value != 'undefined':
+                    network.nodes[node_name]._C = attribute_value
+
+                if attribute_name == 'PCG' and \
+                        attribute_value != 'undefined':
+                    network.nodes[node_name]._PCG = attribute_value
+
+                if attribute_name == 'temporal_groups' and \
+                        attribute_value != 'undefined':
+                    network.nodes[node_name]._temporal_groups = attribute_value
+
+                if attribute_name == 'children' and \
+                        attribute_value != 'undefined':
+                    network.nodes[node_name].children = attribute_value
+
+        return network
+
+    def save (self, uFile, uNetwork):
+        n = uNetwork
+        d = dict ()
+        d['name'] = n.name
+        d['nodes'] = []
+        d['layers'] = []
+
+
+        for (layer_name, list_of_nodes) in n.layers.iteritems ():
+            l = [node.name for node in list_of_nodes]
+            d['layers'].append ({layer_name : l})
+            
+        for (node_name, node_instance) in n.nodes.iteritems ():
+            node = {node_name : dict ()}
+
+            if node_instance._C != []: 
+                node[node_name]['coincidences'] = node_instance._C
+
+            if node_instance._PCG != array ([]): 
+                node[node_name]['PCG'] = node_instance._PCG.tolist ()
+
+            if node_instance._temporal_groups != set ([]): 
+                node[node_name]['temporal_groups'] = \
+                    node_instance._temporal_groups.tolist ()
+
+            node[node_name]['children'] = \
+                [child for child in node_instance.children]
+
+            d['nodes'].append (node)
+
+        f = open(uFile, 'w')
+        f.write (yaml.dump (d))
+        f.close ()
