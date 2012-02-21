ba9049cb11d086d4493b5806cb1a19a01e71bf51
Loris Fichera <loris.fichera@gmail.com>
2012-02-21 15:14:10 +0100

    Added code for entry level nodes.

--- pyhtm/pyhtm.py
+++ pyhtm/pyhtm.py
@@ -4,6 +4,7 @@ An implementation of Hierarchical Temporal Memory in Python.
 """
 
 import time
+import math
 from numpy import *
 from operator import mul
 import yaml
@@ -47,26 +48,59 @@ class Node (object):
 
     def clear_input (self):
         self._lambda_minus = []
-    
-    def feed (self, uLambda, uFrom):
-        self._lambda_minus[uFrom] = uLambda
 
-    def inference (self):
-        ## compute density over coincidences
+    def compute_density_over_coinc (self):
         for c in self._C:
             selected_features = []
 
             for (child,l) in self._lambda_minus.iteritems ():
                 selected_features.append (l[c[child] - 1])
 
+            ## compute each element of y as the multiplication
+            ## of the selected features
             self._y.append (reduce (mul, selected_features))
 
-        ## compute density over temporal groups
-        self._lambda_plus = dot( array (self._y), self._PCG)
+    def compute_density_over_groups (self):
+        self._lambda_plus = dot( array (self._y), self._PCG)        
+    
+    def feed (self, uLambda, uFrom):
+        self._lambda_minus[uFrom] = uLambda
+
+        
+    def inference (self):
+        """Inference is a template method."""
+        compute_density_over_coinc ()
+        compute_density_over_groups ()
+
 
     def propagate (self):
         parent.feed (self._lambda_plus, self.name)
 
+
+class EntryNode (Node):
+    """The EntryNode class. Implements the peculiar input manipulation
+    and inference mechanism of a node beloning to the entry level of 
+    a network.""" 
+    def __init__ (self, uSigma = 1, *args, **kwargs):
+        super(Node, self).__init__(*args, **kwargs)
+
+        self._lambda_minus = array ([[]]) ## small input patch
+        self._sigma = uSigma
+        
+    def feed (self, uInput):
+        """For entry level nodes, the input is a plain numpy array."""
+        self._lambda_minus = uInput
+
+    def inference (self):
+        """For entry level nodes, the y vector is obtained comparing 
+        the coincidence with the input message, by means of the euclidean
+        norm."""
+        for c in self._C:
+            ## compute the euclidean norm with Frobenius formula
+            norm = linalg.norm (c - self._lambda_minus)
+            distance = math.exp ( - math.pow (norm / float (self._sigma), 2))
+            self._y.append (distance)
+            
 
 class Network (object):
     """The Network class. Contains nodes arranged in a hierarchy."""
