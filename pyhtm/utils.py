5d3d9948a3e8db005643df562dfc452b9e968cdd
Loris Fichera <loris.fichera@gmail.com>
2012-02-23 16:33:27 +0100

    load_amat completed.

--- pyhtm/utils.py
+++ pyhtm/utils.py
@@ -1,16 +1,50 @@
+#!/usr/bin/python
 from numpy import array as array
 
-def load_amat (uFilePath):
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def load_amat (uFilePath, uWidth = 0):
     f = open (uFilePath, 'r')
     content = f.readlines ()
     f.close ()
 
     accumulator = []
-    for line in content:
-        row = map (lambda x : float (x), line.split ())
-        accumulator.append (row)
+    if uWidth == 0:
+        for line in content:
+            row = map (lambda x : float (x), line.split ())
+            accumulator.append (row)
+
+        return array (accumulator)
+
+    else:
+        for line in content:
+            #print len(line)
+            subsample = []
+            for chunk in chunks (line.split (), uWidth):
+                row = map (lambda x : float (x), chunk)
+                if len (row) == 1: 
+                    [label] = row
+                    subsample = (label, array (subsample))
+                    break
+                subsample.append (array (chunk) )
+    
+            accumulator.append (subsample)
         
-    return array (accumulator)
+        return accumulator
+
+
+def test():
+    l = load_amat ('/home/loris/Downloads/rectangles_train.amat', 28)
+    print l
+
+if __name__ == "__main__":
+    test ()
+    
+    
     
     
         
